9b5f693217af7cfabb7d48025a86c0c89c03b464
Julian Sanchez <juliansanchezv96@gmail.com>
2023-08-30 17:03:30 -0500

    updated home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  text1: string = '¡Hola';
+  text2: string = '! Soy Julián';
+  text3: string = 'Desarrollador Front End';
 }
